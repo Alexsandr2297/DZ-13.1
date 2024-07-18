a290e8001c24fc54f4dc43e00de36f1d534c92a4
Alexsandr Varenyshev <warrn11@mail.ru>
2024-07-18 22:25:24 +0300

    Проект написан

---
+++ src/main.py
@@ -0,0 +1,29 @@
+class Category:
+    """Класс для Категории"""
+    title: str  # Название
+    description: str  # Описание
+    products: list  # Товары
+
+    def __init__(self, title, description, products):
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
+        self.title = title
+        self.description = description
+        self.products = products
+
+        total_number_of_categories = 0  # общее количество категорий
+        total_number_of_unique_products = 0  # общее количество уникальных продуктов
+
+
+class Product:
+    """Класс для Продукта"""
+    title: str  # Название
+    description: str  # Описание
+    price: float  # Цена
+    quantity: int  # количество в наличии
+
+    def __init__(self, title, description, price, quantity):
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра."""
+        self.title = title
+        self.description = description
+        self.price = price
+        self.quantity = quantity
---
+++ tests/test_code.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.main import Category
+from src.main import Product
+
+
+@pytest.fixture()
+def category_title():
+    return Category('Настольные игры', 'Для веселого времяпровождения в компании', 'Зомби в доме, '
+                                                                                   'Имаджинариум, Экивоки')
+
+
+def test_init(category_title):
+    assert category_title.title == "Настольные игры"
+    assert category_title.description == "Для веселого времяпровождения в компании"
+    assert category_title.products == "Зомби в доме, Имаджинариум, Экивоки"
+
+
+@pytest.fixture()
+def product_title():
+    return Product('Настольные игры', 'Для веселого времяпровождения в компании', 1999.99, 10)
+
+
+def test_init2(product_title):
+    assert product_title.title == "Настольные игры"
+    assert product_title.description == "Для веселого времяпровождения в компании"
+    assert product_title.price == 1999.99
+    assert product_title.quantity == 10
