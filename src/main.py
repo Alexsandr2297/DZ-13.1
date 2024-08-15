3369d1f3ca7001da5754bcb181582c5cb6a6078c
Alexsandr Varenyshev <warrn11@mail.ru>
2024-08-15 23:09:55 +0300

    DZ-14_test_add_product

--- src/main.py
+++ src/main.py
@@ -22,6 +22,14 @@ class Category:
         return [f"{product.title}, {product.price} руб. Остаток:{product.quantity} шт." for product in
                 self.__products]
 
+    def __str__(self):
+        product_count = len(self.__products)
+        self.category_name = self.title
+        return f'{self.category_name}, количество продуктов: {product_count} шт.'
+
+    def __iter__(self):
+        return CategoryIterator(self.__products)
+
 
 class Product:
     """Класс для Продукта"""
@@ -40,3 +48,46 @@ class Product:
     @classmethod
     def product(cls, name, description, price, quantity):
         return cls(name, description, price, quantity)
+
+    def __str__(self):
+        return f"{self.title}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        return (self.price * self.quantity) + (other.price * other.quantity)
+
+
+class CategoryIterator:
+    """Итератор для класса Category"""
+
+    def __init__(self, products):
+        self._products = products
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index < len(self._products):
+            product = self._products[self._index]
+            self._index += 1
+            return product
+        else:
+            raise StopIteration
+
+    # category1 = Category('Настольные игры', 'Категория с настольными играми',
+    #                      [Product('Зомби в доме', 'Для веселого времяпровождения в компании', 1999.99, 10),
+    #                       Product('Имаджинариум', 'Для веселого времяпровождения в компании', 2999.99, 5),
+    #                       Product('Экивоки', 'Для веселого времяпровождения в компании', 3999.99, 3)])
+    #
+    # # Вывожу информацию о категории
+    #
+    # product1 = Product('Зомби в доме', 'Для веселого времяпровождения в компании', 1999.99, 10)
+    # product2 = Product('Имаджинариум', 'Для веселого времяпровождения в компании', 2999.99, 5)
+    #
+    # # Добавляю продукты в категорию
+    # category1.add_products(product1)
+    # category1.add_products(product2)
+    #
+    # # Запускаю итерацию по продуктам в категории
+    # for product in category1:
+    #     print(product)
--- tests/test_code.py
+++ tests/test_code.py
@@ -33,6 +33,15 @@ def test_add_product(category_title):
     assert category_title.products[-1] == 'Монополия, 1999.99 руб. Остаток:12 шт.'
 
 
+def test__category_str__(category_title):
+    product_count = Category("Настольные игры", "Для веселого времяпровождения в компании",
+                             [Product('Зомби в доме', 'Для веселого времяпровождения в компании', 1999.99, 10),
+                              Product('Имаджинариум', 'Для веселого времяпровождения в компании', 2999.99, 5),
+                              Product('Экивоки', 'Для веселого времяпровождения в компании', 3999.99, 3)])
+    result = "Настольные игры, количество продуктов: 3 шт."
+    assert str(product_count) == result
+
+
 @pytest.fixture()
 def product_title():
     return Product('Зомби в доме', 'Для веселого времяпровождения в компании', 1999.99, 10)
@@ -50,3 +59,18 @@ def test_products(product_title):
     assert product_title.description == "Для веселого времяпровождения в компании"
     assert product_title.price == 1999.99
     assert product_title.quantity == 10
+
+
+def test_product_str_(product_title):
+    result = "Зомби в доме, 1999.99 руб. Остаток: 10 шт."
+    assert str(product_title) == result
+
+
+def test_product_add(product_title):
+    # Создаем два объекта
+    product1 = Product('Зомби в доме', 'Для веселого времяпровождения в компании', 1999.99, 10)
+    product2 = Product('Имаджинариум', 'Для веселого времяпровождения в компании', 2999.99, 5)
+
+    expected_sum = 1999.99 * 10 + 2999.99 * 5
+
+    assert (product1 + product2) == expected_sum
