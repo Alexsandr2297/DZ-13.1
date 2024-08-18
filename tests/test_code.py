ef8be6401663c36e534c4e5a69788f0bfbeb768c
Alexsandr Varenyshev <warrn11@mail.ru>
2024-08-18 19:14:44 +0300

    DZ-14_test_add_test_iterator

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.main import Category
+from src.main import CategoryIterator
 from src.main import Product
 
 
@@ -17,6 +18,7 @@ def test_init(category_title):
     assert category_title.description == "Для веселого времяпровождения в компании"
     assert len(category_title.products) == 3
     assert Category.total_number_of_categories == 1
+    assert Category.total_number_of_unique_products == 3
 
 
 def test_add_product(category_title):
@@ -40,6 +42,21 @@ def test__category_str__(category_title):
                               Product('Экивоки', 'Для веселого времяпровождения в компании', 3999.99, 3)])
     result = "Настольные игры, количество продуктов: 3 шт."
     assert str(product_count) == result
+
+
+def test_iter():
+    products = [
+        Product("Зомби в доме", "Для веселого времяпровождения в компании", 1999.99, 10),
+        Product("Имаджинариум", "Для веселого времяпровождения в компании", 2999.99, 5),
+        Product('Экивоки', 'Для веселого времяпровождения в компании', 3999.99, 3)
+    ]
+    category_iterator = CategoryIterator(products)
+    assert next(category_iterator).title == "Зомби в доме"
+    assert next(category_iterator).title == "Имаджинариум"
+    assert next(category_iterator).title == "Экивоки"
+
+    with pytest.raises(StopIteration):
+        next(category_iterator)
 
 
 @pytest.fixture()
@@ -74,3 +91,25 @@ def test_product_add(product_title):
     expected_sum = 1999.99 * 10 + 2999.99 * 5
 
     assert (product1 + product2) == expected_sum
+
+
+def test_iterator():
+    # Пустой список
+    products = CategoryIterator([])
+    with pytest.raises(StopIteration):
+        next(products)
+
+    # Список с одним продуктом
+    product1 = Product("Зомби в доме", "Для веселого времяпровождения в компании", 1999.99, 10)
+    products = CategoryIterator([product1])
+    assert next(products) == product1
+    with pytest.raises(StopIteration):
+        next(products)
+
+    # Список с несколькими продуктами
+    product2 = Product("Имаджинариум", "Для веселого времяпровождения в компании", 2999.99, 5)
+    products = CategoryIterator([product1, product2])
+    assert next(products) == product1
+    assert next(products) == product2
+    with pytest.raises(StopIteration):
+        next(products)
